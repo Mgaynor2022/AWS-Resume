4d664a5d0535cffb9062d98f85d3a5ff395dccff
Michael Gaynor <mgaynor228@gmail.com>
2024-01-04 00:37:44 -0600

    updated lambda function

--- aws-resume/visitorCounter/hello_world/app.py
+++ aws-resume/visitorCounter/hello_world/app.py
@@ -3,12 +3,26 @@ import boto3
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb('vistorCounter')
-def lambda_handler(event, context):
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+def lambda_handler(event, context):
+    
+    response = table.get_item( key= {
+        'id':'1'
+    })
+    visitor = response['item']['visitor']
+    visitor = visitor + 1
+    print(visitor)
+
+    response = table.put_item(item = {
+        'id':'2',
+        'visitor': visitor
+    })
+    return visitor
+
+    # return {
+    #     "statusCode": 200,
+    #     "body": json.dumps({
+    #         "message": "hello world",
+    #         # "location": ip.text.replace("\n", "")
+    #     }),
+    # }
